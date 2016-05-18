8cf9a220d7d5438edbe81f8d3288b4bf746ac6fd
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2016-05-18 11:14:09 -0400

    added templates and routes in config

--- src/js/config.js
+++ src/js/config.js
@@ -1,8 +1,27 @@
 function config($stateProvider,$urlRouterProvider) {
   
   // console.log($stateProvider);
+  // if non existing state go home
   $urlRouterProvider.otherwise('/');
 
+  // define all states  takes a name and object
+  $stateProvider
+    .state('list',{
+      url         :'/',
+      controller  :'ListController',
+      templateUrl :'templates/list.tpl.html'
+    })
+    .state('single',{
+      url         :'/single',
+      controller  :'SingleController',
+      templateUrl : 'templates/single.tpl.html'
+    })
+    .state('add',{
+      url         :'/add',
+      controller  :'AddController',
+      templateUrl :'templates/add.tpl.html'
+    })
+  ;
   
 
 };
